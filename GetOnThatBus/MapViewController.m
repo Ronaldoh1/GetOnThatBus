215180b07c94581c2f6ba3d331f0d2448eab9502
Ronald Hernandez <2ronald.hernandez@gmail.com>
2015-03-26 17:36:20 -0700

    able to display a list and map controlled by segmented control

--- GetOnThatBus/MapViewController.m
+++ GetOnThatBus/MapViewController.m
@@ -13,14 +13,16 @@
 #import "BusStopDetailViewController.h"
 #import "CustomMKPointAnnotation.h"
 
-@interface MapViewController ()<MKMapViewDelegate, BusStopDownloaderDelegate>
+@interface MapViewController ()<MKMapViewDelegate, BusStopDownloaderDelegate, UITableViewDelegate, UITableViewDataSource>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property BusStopDownloader *downloader;
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property NSArray *busStopsArray;
 @property NSMutableArray *arrayOfBusStopObjects;
 
+@property (weak, nonatomic) IBOutlet UISegmentedControl *SegmentedController;
 
 
 @end
@@ -30,12 +32,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
-     self.downloader = [BusStopDownloader new];
+    self.downloader = [BusStopDownloader new];
     self.downloader.parentVC = self;
     [self.downloader pullBusStopsFromCTAApi];
 
     //allocate and initialize dictionary for bus stop objects.
-  self.arrayOfBusStopObjects = [[NSMutableArray alloc]init];
+    self.arrayOfBusStopObjects = [[NSMutableArray alloc]init];
 
     self.navigationController.navigationBar.backgroundColor = [UIColor redColor];
 
@@ -53,11 +55,12 @@
         [self.arrayOfBusStopObjects addObject:busStop];
     }
 
-     [self displayPins:self.arrayOfBusStopObjects];
+    [self displayPins:self.arrayOfBusStopObjects];
+    [self.tableView reloadData];
 
 }
 
-    //--Display pins---//
+//--Display pins---//
 -(void)displayPins:(NSArray *)busStopsArray{
 
 
@@ -83,7 +86,7 @@
     }
 
 }
-    //--Zoom into region--//
+//--Zoom into region--//
 -(void)zoomToRegion:(double *)lat :(double *)lon{
 
     MKCoordinateRegion region;
@@ -95,7 +98,7 @@
     [self.mapView setRegion:region animated:TRUE];
 
 }
-    //---Prepare for segue---//
+//---Prepare for segue---//
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
 
     if([segue.identifier isEqualToString:@"toDetailPage"]){
@@ -104,13 +107,13 @@
         DestinationVC.busStop = pointAnnotation.busStop;
 
     }
-    
+
 }
 
-#pragma mark MapKit Delegate
+#pragma mark MapKit-Delegate
 - (void)mapView:(MKMapView *)mapView annotationView:(MKAnnotationView *)view calloutAccessoryControlTapped:(UIControl *)control{
-   
-        [self performSegueWithIdentifier:@"toDetailPage" sender:view.annotation];
+
+    [self performSegueWithIdentifier:@"toDetailPage" sender:view.annotation];
 
 }
 
@@ -134,6 +137,40 @@
     pinAnnotation.rightCalloutAccessoryView = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
 
     return  pinAnnotation;
+}
+#pragma mark MapKit-Delegate
+
+
+-(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"busStopCell"];
+
+    cell.textLabel.text = [self.arrayOfBusStopObjects[indexPath.row]stopName];
+
+    return cell;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+
+
+    return self.arrayOfBusStopObjects.count;
+//    return 10;
+}
+
+- (IBAction)onSegmentedControlTapped:(UISegmentedControl *)sender {
+
+    if (sender.selectedSegmentIndex == 0) {
+        self.mapView.hidden = false;
+        self.tableView.hidden = true;
+         NSLog(@"Map");
+
+    }else if(sender.selectedSegmentIndex == 1){
+        [self.mapView isHidden];
+        self.mapView.hidden = true;
+        self.tableView.hidden = false;
+         NSLog(@"Table");
+        [self.tableView reloadData];
+
+    }
 }
 
 
